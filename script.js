400d23218eda68cb9199708fbb9f10a18e9ff4cf
t-powers <aw.powers@hotmail.com>
2023-08-10 20:52:50 -0400

    saving spot

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let bigName = "Big Poppa";
-alert(`Hi, ${bigName}`);
+const button = document.querySelector("button");
+
+function greet() {
+  const name = prompt("What is your name?");
+  alert(`Hello ${name}, nice to see you!`);
+}
+
+button.addEventListener("click", greet);
