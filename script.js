9ef401dc8689dce476dec486d89ae5d7b7a7e33d
t-powers <aw.powers@hotmail.com>
2023-08-14 19:59:29 -0400

    saving this one - js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let x = 10;
+const foo = { name: "tom", age: 30, nervous: false };
+const bar = { name: "dick", age: 31, nervous: false };
+const baz = { name: "harry", age: 32, nervous: true };
 
-if (x > 5) {
-  console.log(`Yes, ${x} is greater.`);
-} else {
-  console.log(`No, ${x} is less than.`);
-}
+console.log("%c My Friends", "color: orange;font-weight: bold");
+console.log({ foo, bar, baz });
