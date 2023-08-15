1f0b510fca73f05a8b173c1d58d8ca0946029d3c
t-powers <aw.powers@hotmail.com>
2023-08-15 07:28:06 -0400

    practising console.log

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const foo = { name: "tom", age: 30, nervous: false };
-const bar = { name: "dick", age: 31, nervous: false };
-const baz = { name: "harry", age: 32, nervous: true };
+// const foo = { name: "tom", age: 30, nervous: false };
+// const bar = { name: "dick", age: 31, nervous: false };
+// const baz = { name: "harry", age: 32, nervous: true };
 
-console.log("%c My Friends", "color: orange;font-weight: bold");
-console.log({ foo, bar, baz });
+// console.log("%c My Friends", "color: orange;font-weight: bold");
+// console.table([foo, bar, baz])
+
+const a = 2;
+if (a > 2) {
+  console.log("Greater than 2.");
+}
