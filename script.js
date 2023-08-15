6f1c551e5bfc23fd815370454e28cdb18c70d133
t-powers <aw.powers@hotmail.com>
2023-08-15 19:44:17 -0400

    study

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// const foo = { name: "tom", age: 30, nervous: false };
-// const bar = { name: "dick", age: 31, nervous: false };
-// const baz = { name: "harry", age: 32, nervous: true };
-
-// console.log("%c My Friends", "color: orange;font-weight: bold");
-// console.table([foo, bar, baz])
-
-let myself = "Daddy";
-if ((myself[0] = "D")) alert("D is for Daddy");
-console.log(myself);
+let a = prompt("How old are you?");
+if (a > 1) {
+  console.log(true);
+}
