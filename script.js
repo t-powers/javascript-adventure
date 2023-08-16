176e4763d1258375e63217e850bb8aaa24e9d030
t-powers <aw.powers@hotmail.com>
2023-08-15 20:24:38 -0400

    creating password system

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let a = prompt("How old are you?");
-if (a > 1) {
-  console.log(true);
+// entering password
+let visitor = prompt("Who's there?");
+
+if (visitor == "Admin") {
+  let password = prompt("Add password.");
+
+  if (password == "TheMaster") {
+    alert("Welcome");
+  }
+} else if (visitor == "" || visitor == null) {
+  alert("Wrong Password.");
+} else {
+  alert("Access Denied");
 }
