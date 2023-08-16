7338da7b287ec50d1585526401e82ba7fb60e889
t-powers <aw.powers@hotmail.com>
2023-08-16 06:47:06 -0400

    practising

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Practising console logging table
+
 // const foo = { name: "tom", age: 30, nervous: false };
 // const bar = { name: "dick", age: 31, nervous: false };
 // const baz = { name: "harry", age: 32, nervous: true };
@@ -5,6 +7,8 @@
 // console.log("%c My Friends", "color: orange;font-weight: bold");
 // console.table([foo, bar, baz])
 
-let myself = "Daddy";
-if ((myself[0] = "D")) alert("D is for Daddy");
-console.log(myself);
+//Practising console logging if statement
+
+// let myself = "Daddy";
+// if ((myself[0] = "D")) alert("D is for Daddy");
+// console.log(myself);
