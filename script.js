88e57b0b494ab933d874aaa5fafdaf70ef48efa3
t-powers <aw.powers@hotmail.com>
2023-08-16 06:58:52 -0400

    logging practice

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-//Practising console logging table
+// // entering password
+// let visitor = prompt("Who's there?");
 
-// const foo = { name: "tom", age: 30, nervous: false };
-// const bar = { name: "dick", age: 31, nervous: false };
-// const baz = { name: "harry", age: 32, nervous: true };
+// if (visitor == "Admin") {
+//   let password = prompt("Add password.");
 
-// console.log("%c My Friends", "color: orange;font-weight: bold");
-// console.table([foo, bar, baz])
-
-//Practising console logging if statement
-
-// let myself = "Daddy";
-// if ((myself[0] = "D")) alert("D is for Daddy");
-// console.log(myself);
+//   if (password == "TheMaster") {
+//     alert("Welcome");
+//   }
+// } else if (visitor == "" || visitor == null) {
+//   alert("Wrong Password.");
+// } else {
+//   alert("Access Denied");
+// }
+=2+2
