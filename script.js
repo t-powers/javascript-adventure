4472d874a09c8a37c455a29e213af57438e37032
t-powers <aw.powers@hotmail.com>
2023-08-16 07:07:21 -0400

    adding completed replit code to file to study

--- script.js
+++ script.js
@@ -5,6 +5,42 @@
 // console.log("%c My Friends", "color: orange;font-weight: bold");
 // console.table([foo, bar, baz])
 
+// practice
+// let userAge = prompt("What is your age?");
+
+// if (userAge >=14 && userAge <=90) {
+//   alert("Your ticket will be $1,000,000.");
+// }
+// else{
+//   alert("Come back next year.")
+// }
+
+// practice
+// let userAge = prompt("What is your age?");
+
+// if (!(userAge >=14 && userAge <=90)) {
+//   alert("You are not greater than 14 and less than 90.");
+// }
+// else{alert("You're 14 to 90 years old. Yikes.");}
+
+// entering password
+// let visitor = prompt("Who's there?");
+
+// if (visitor == "Admin") {
+
+//   let password = prompt("Add password.");
+
+//   if (password == "TheMaster") {
+//     alert("Welcome");
+//   }
+// }
+// else if (visitor == '' || visitor == null) {
+//   alert("Wrong Password.");
+// }
+// else {
+//   alert("Access Denied");
+// }
+
 let myself = "Daddy";
 if ((myself[0] = "D")) alert("D is for Daddy");
 console.log(myself);
