56033b9ff13cfcca1311477eedffe80fa3544726
t-powers <127955989+t-powers@users.noreply.github.com>
2023-09-07 21:19:57 -0400

    installed node prompt-sync

--- script.js
+++ script.js
@@ -130,3 +130,8 @@
 //   console.log(`Hey there ${name}!`);
 //   readline.close();
 // });
+
+// const prompt = require("prompt-sync")();
+
+// const name = prompt("What is your name? ");
+// console.log(`Hey there ${name}`);
