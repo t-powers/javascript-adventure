cb5ae2bd313d9e2a4dbfcaa19382c85569a5c3da
t-powers <127955989+t-powers@users.noreply.github.com>
2023-09-16 23:20:01 -0400

    testing js problem

--- script.js
+++ script.js
@@ -146,12 +146,3 @@
 //   }
 // }
 // game();
-
-const newFunc = function (array, ...args) {
-  let a = array;
-  let b = args;
-  let c = a.filter((d) => !b.include(d));
-  return c;
-};
-
-console.log(newFunc([1, 2, 3], 3, 2));
