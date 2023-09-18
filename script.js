510fe10e50caf6ebdcc2dca115f58f24c2ee6f84
t-powers <127955989+t-powers@users.noreply.github.com>
2023-09-18 09:16:40 -0400

    testing assumptions to sumAll problem

--- script.js
+++ script.js
@@ -101,13 +101,14 @@
 // console.log(xy(3, 2));
 
 // let xy = function sumNum(a, ...b) {
-//   return 2 * (a + b);
+//   return (a + b);
 // };
-// console.log(xy(1, 2));
+// console.log(xy(4, 2));
 
-// const calcNumbers = (a, b) => a * b;
-// console.log(calcNumbers(2, 3));
-// console.log(calcNumbers(5, 5));
+const calcNumbers = (a, b) => a + b;
+
+console.log(calcNumbers(2, 3));
+console.log(calcNumbers(5, 3));
 
 // const calcFloat = (a, b) => a * b;
 // console.log(calcFloat(8, 3));
