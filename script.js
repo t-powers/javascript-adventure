5591a67e40cc31c61409bd206c5aee6558256765
t-powers <anthony_powers@icloud.com>
2025-05-31 22:00:07 -0400

    code cleanup

--- script.js
+++ script.js
@@ -322,4 +322,23 @@
 
 // }
 
+// create new array of objects, use name and surname to create fullname, include id.
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
 
+let users = [ john, pete, mary ];
+
+let usersMapped = /* ... your code ... */
+//take 'users' and map a new array
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+*/
+
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // John Smith
