12673d6970109d2f46be5e924fdd7692d405579b
t-powers <127955989+t-powers@users.noreply.github.com>
2025-06-01 21:30:47 -0400

    Update

--- script.js
+++ script.js
@@ -322,23 +322,45 @@
 
 // }
 
-// create new array of objects, use name and surname to create fullname, include id.
-let john = { name: "John", surname: "Smith", id: 1 };
-let pete = { name: "Pete", surname: "Hunt", id: 2 };
-let mary = { name: "Mary", surname: "Key", id: 3 };
+// // create new array of objects, use name and surname to create fullname, include id.
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
 
-let users = [ john, pete, mary ];
+// let users = [john, pete, mary];
 
-let usersMapped = /* ... your code ... */
-//take 'users' and map a new array
+// let usersMapped = users.map((user) => {
+//   return {
+//     fullName: `${user.name} ${user.surname}, ${user.id}`,
+//     id: `${user.id}`,
+//   };
+// });
+// //take 'users' and map a new array
 
-/*
-usersMapped = [
-  { fullName: "John Smith", id: 1 },
-  { fullName: "Pete Hunt", id: 2 },
-  { fullName: "Mary Key", id: 3 }
-]
-*/
+// console.log( usersMapped[2].id ) // 1
+// console.log(usersMapped[2].fullName); // John Smith
 
-alert( usersMapped[0].id ) // 1
-alert( usersMapped[0].fullName ) // John Smith
+// /*
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 }
+// ]
+// */
+
+// function sortByAge(arr) {
+//   return arr.sort((a, b) => a.age - b.age);
+// }
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [pete, john, mary];
+
+// sortByAge(arr);
+
+// // now: [john, mary, pete]
+// console.log(arr[0].name); // John
+// console.log(arr[1].name); // Mary
+// console.log(arr[2].name); // Pete
