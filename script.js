4d89b5c2d94fc79c6e6cd0225cb851716549e249
t-powers <127955989+t-powers@users.noreply.github.com>
2025-06-02 15:20:31 -0400

    Update

--- script.js
+++ script.js
@@ -376,3 +376,27 @@
 // }
 
 // console.log( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+// let users = [
+//   { id: "john", name: "John Smith", age: 20 },
+//   { id: "ann", name: "Ann Smith", age: 24 },
+//   { id: "pete", name: "Pete Peterson", age: 31 },
+// ];
+
+// const groupById = (array) =>
+//   array.reduce((obj, value) => ({ ...obj, [value.id]: value }), {});
+
+// let usersById = groupById(users);
+
+// console.log(usersById);
+
+
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
